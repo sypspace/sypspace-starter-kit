be503fb2ed893408e5bd6ee2ff73b89044556f58
Sukmono Yogi <sukmono.yogi@gmail.com>
2024-04-01 22:59:03 +0700

    HasPageShield trait has default permission access - canView()

--- app/Filament/Pages/MyProfilePage.php
+++ app/Filament/Pages/MyProfilePage.php
@@ -9,9 +9,4 @@ use Jeffgreco13\FilamentBreezy\Pages\MyProfilePage as MyProfilePageBase;
 class MyProfilePage extends MyProfilePageBase
 {
     use HasPageShield;
-
-    public static function canAccess(): bool
-    {
-        return auth()->user()->can('page_MyProfilePage');
-    }
 }
